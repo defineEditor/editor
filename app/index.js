218283d55bb8c88b8a7bee1a04afda94236cfc78
Dmitry <dmitry.m.kolosov@gmail.com>
2018-07-27 00:52:45 +0300

    Moving to electron-react-boilerplate

---
+++ app/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import unhandled from 'electron-unhandled';
+import { ipcRenderer } from 'electron';
+import store from 'store/index.js';
+import App from 'core/app.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+import 'css/index.css';
+
+// Stardard events
+const sendDefineObject = (error, data) => {
+    let odm = store.getState().odm;
+    window.ipcRenderer.send('DefineObject', odm);
+};
+
+ipcRenderer.on('SendDefineObjectToMain', sendDefineObject);
+
+// Handle unhandled errors;
+unhandled();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
