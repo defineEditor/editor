0d0552d45c319498a09ddb77221fd75a9894340d
Dmitry <dmitry.m.kolosov@gmail.com>
2018-12-10 23:00:11 +0300

    Adding license

--- app/actions/standard.js
+++ app/actions/standard.js
@@ -1,3 +1,17 @@
+/***********************************************************************************
+* This file is part of Visual Define-XML Editor. A program which allows to review  *
+* and edit XML files created using CDISC Define-XML standard.                      *
+* Copyright (C) 2018 Dmitry Kolosov                                                *
+*                                                                                  *
+* Visual Define-XML Editor is free software: you can redistribute it and/or modify *
+* it under the terms of version 3 of the GNU Affero General Public License         *
+*                                                                                  *
+* Visual Define-XML Editor is distributed in the hope that it will be useful,      *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY   *
+* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License   *
+* version 3 (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.           *
+***********************************************************************************/
+
 import {
     UPD_GLOBALVARSSTOID,
     UPD_ODMATTRS,
