8294f483c1f6db442e5dda2698501c4d346c12f7
Dmitry <dmitry.m.kolosov@gmail.com>
2019-01-24 23:19:29 +0300

    Minor fixes

--- app/constants/help.js
+++ app/constants/help.js
@@ -16,15 +16,15 @@ export const CODELIST_POPULATESTD = {
     title: 'Populate Standards Codelists',
     content:`
 ### About
-Each codelist can be connected to a codelist from the standard Controlled Terminology.
+Each codelist can be connected to a codelist from a standard Controlled Terminology.
 ### Match Options
-* **Match by name**. Codelist names are be compared with each other. It is possible use the following options which are applied to both values:
+* **Match by name**. Codelist names are be compared with each other. It is possible to use the following options which are applied on both sides:
   * **Match Case**. When disabled '**No Yes Reponse**' matches '**No yes response**'
   * **Ignore Whitespaces**. (including trailing and leading spaces). When enabled '**No Yes Reponse**' matches '** No   YesResponse   **'
-  * **Exclude Pattern**. A regular expression used to remove part of the codelist name before comparison.
- The default value **\\s*\\(.*\\)\\s*$** removes the last parenthesis.
- When specified '**No Yes Reponse**' matches '**No Yes Response (Y Subset)**'
-* **Match by C-Code**. In case imported Define-XML has standard CT C-Codes specified for the codelists,
- it will be used to select corresponding codelists in the Standard CT.
+  * **Exclude Pattern**. A regular expression used to a remove part of the codelist name before the comparison.
+ Default value **\\s*\\(.*\\)\\s*$** removes the last parentheses.
+ When specified, '**No Yes Reponse**' matches '**No Yes Response (Y Subset)**'
+* **Match by C-Code**. In case the imported Define-XML has a standard C-Code specified for the codelists,
+ it will be used to select a corresponding codelist in the standard Controlled Terminology.
 `
 };
