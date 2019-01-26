3cf6bf8f2d97ad77f430d1d05e50857e9d84c12b
Sergei Krivtcov <1000eyed@gmail.com>
2019-01-26 23:47:55 +0100

    Changes to propose to Dmitry: 1. modalPopulateStdCodeLists: checkboxes in 'match by value' section are now in proper case instead of camel case; 2. help: same as 1. Punctuation at the end of each checkbox in 'match by value' section.

--- app/constants/help.js
+++ app/constants/help.js
@@ -19,11 +19,11 @@ export const CODELIST_POPULATESTD = {
 Each codelist can be connected to a codelist from a standard Controlled Terminology.
 ### Match Options
 * **Match by name**. Codelist names are be compared with each other. It is possible to use the following options which are applied on both sides:
-  * **Match Case**. When disabled '**No Yes Reponse**' matches '**No yes response**'
-  * **Ignore Whitespaces** (including trailing and leading spaces). When enabled '**No Yes Reponse**' matches '** No   YesResponse   **'
-  * **Exclude Pattern**. A regular expression used to a remove part of the codelist name before the comparison.
+  * **Match case**. When disabled '**No Yes Reponse**' matches '**No yes response**';
+  * **Ignore whitespaces** (including trailing and leading spaces). When enabled '**No Yes Reponse**' matches '** No   YesResponse   **';
+  * **Exclude pattern**. A regular expression used to a remove part of the codelist name before the comparison.
  Default value **\\s*\\(.*\\)\\s*$** removes the last parentheses.
- When specified, '**No Yes Reponse**' matches '**No Yes Response (Y Subset)**'
+ When specified, '**No Yes Reponse**' matches '**No Yes Response (Y Subset)**'.
 * **Match by C-Code**. In case the imported Define-XML has a standard C-Code specified for the codelists,
  it will be used to select a corresponding codelist in the standard Controlled Terminology.
 `
